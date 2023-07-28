d226a08446c00b0b9b4825bbd596e68b9817f84a
PromiSe#### <vicimpa@ya.ru>
2023-07-28 07:34:11 +0700

    Fix config

--- src/lib/dir.js
+++ src/lib/dir.js
@@ -1,5 +1,10 @@
 const { join } = require("path");
 
+/**
+ * 
+ * @param {...string} segments 
+ * @returns 
+ */
 exports.dir = function dir(...segments) {
   return join(__dirname, '../', ...segments);
 };
